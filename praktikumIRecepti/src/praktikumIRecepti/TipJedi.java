3e80d14c3d99aed44d621e5cc9522141b8586936
Ivona <ivona.colakovic1@gmail.com>
2018-05-18 12:39:17 +0200

    Dodan projekt

---
+++ praktikumIRecepti/src/praktikumIRecepti/TipJedi.java
@@ -0,0 +1,22 @@
+package praktikumIRecepti;
+
+public class TipJedi {
+	
+	private int idTipJedi;
+	private String naziv;
+	public int getIdTipJedi() {
+		return idTipJedi;
+	}
+	public void setIdTipJedi(int idTipJedi) {
+		this.idTipJedi = idTipJedi;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	
+	
+	
+}
